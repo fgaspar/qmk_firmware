c63ad79115f14475b8c77bc957d2338b90de8024
Francisco Gaspar <dragaoazulg@gmail.com>
2023-02-12 11:46:31 +0000

    Adding my own keyboard config

---
+++ keyboards/crkbd/keymaps/fgaspar/rules.mk
@@ -0,0 +1,9 @@
+RGBLIGHT_ENABLE    = no
+RGB_MATRIX_ENABLE  = yes
+MOUSEKEY_ENABLE    = yes
+NKRO_ENABLE        = yes
+OLED_ENABLE        = yes
+OLED_DRIVER        = SSD1306
+LTO_ENABLE         = yes
+CONVERT_TO         = elite_pi
+#TAP_DANCE_ENABLE   = yes
